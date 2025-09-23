8b48f187e7003daffe434b385810065fcafe97ef
gavasarcenio-crypto <ga.vas.arcenio@gmail.com>
2025-09-23 13:47:36 -0300

    dbnv

--- interlibras/src/components/TranslationBox.jsx
+++ interlibras/src/components/TranslationBox.jsx
@@ -1,10 +1,24 @@
+"use client";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/TranslationBox.module.css";
 
 export default function TranslationBox() {
+    const [translation, setTranslation] = useState("Carregando...");
+
+    useEffect(() => {
+    // 🔹 Aqui futuramente você vai chamar o backend
+    // Por enquanto vou simular uma atualização a cada 3s
+    const interval = setInterval(() => {
+        setTranslation((prev) => prev + " bla");
+    }, 3000);
+
+    return () => clearInterval(interval);
+    }, []);
+
     return (
-    <div className={styles.translation}>
+    <div className={styles.translationBox}>
         <h3>Tradução</h3>
-        <p>bla bla bla bla bla bla bla bla...</p>
+        <p>{translation}</p>
     </div>
     );
 }

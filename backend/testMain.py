1327742501a35f586de101139832072f24e8450a
Gabriel Hugi <gabrielhugi2@gmail.com>
2025-09-23 18:13:06 -0300

    funciona a detecção de mão

--- backend/testMain.py
+++ backend/testMain.py
@@ -4,29 +4,37 @@
 
 import cv2
 import mediapipe as mp
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles=mp.solutions.drawing_styles
-mphands = mp.solutions.hands
+
+mp_maos = mp.solutions.hands
+mp_desenho = mp.solutions.drawing_utils
+maos = mp_maos.Hands(max_num_hands=2, min_detection_confidence=0.7, min_tracking_confidence=0.5)
 
 cap = cv2.VideoCapture(0)
-hands = mphands.Hands()
 
 while True:
-    data, image = cap.read()
+    sucesso, imagem = cap.read()
+    if not sucesso:
+        print("Ignorando frame vazio da câmera.")
+        continue
 
-    if not data:
-        print("❌ Erro ao acessar a câmera")
-        break
+    imagem = cv2.flip(imagem, 1)
+    imagem_rgb = cv2.cvtColor(imagem, cv2.COLOR_BGR2RGB)
 
-    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
+    resultados = maos.process(imagem_rgb)
 
-    # Mostra o frame na tela (para feedback visual)
-    image_bgr = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    cv2.imshow("Webcam", image_bgr)
+    imagem = cv2.cvtColor(imagem_rgb, cv2.COLOR_RGB2BGR)
 
-    # Encerra com 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if resultados.multi_hand_landmarks:
+        for landmarks_mao in resultados.multi_hand_landmarks:
+            mp_desenho.draw_landmarks(
+                imagem,
+                landmarks_mao,
+                mp_maos.HAND_CONNECTIONS
+            )
+
+    cv2.imshow("Detector de Mãos - MediaPipe", imagem)
+
+    if cv2.waitKey(5) & 0xFF == ord('q'):
         break
 
 cap.release()
